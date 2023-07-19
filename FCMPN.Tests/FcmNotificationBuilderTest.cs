81bf0042a7b8fb759c95f6c5c730087ace52fb06
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2023-07-19 14:45:49 +0200

    unit tests added

---
+++ FCMPN.Tests/FcmNotificationBuilderTest.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+
+public class FcmNotificationBuilderTest
+{
+    [Fact]
+    public void TestBuildMessage()
+    {
+        var content = new FcmNotificationBuilder.NotificationContent
+        {
+            accRef = "accRef",
+            eventName = "eventName",
+            payload = new FcmNotificationBuilder.PayloadContent { someProperty = "someValue" },
+            sentAt = "2021-01-01T00:00:00.000Z"
+        };
+
+        var message = FcmNotificationBuilder.Create()
+            .AddContent(content)
+            .SetNotificationText("title", "body")
+            .SetTag(1)
+            .AddToken("deviceToken")
+            .Build();
+
+        Assert.Equal("deviceToken", message.Token);
+
+        Assert.True(message.Data.ContainsKey("content"));
+        Assert.Equal(JsonConvert.SerializeObject(content), message.Data["content"]);
+
+        Assert.True(message.Data.ContainsKey("title"));
+        Assert.Equal("title", message.Data["title"]);
+
+        Assert.True(message.Data.ContainsKey("body"));
+        Assert.Equal("body", message.Data["body"]);
+
+        Assert.True(message.Data.ContainsKey("tag"));
+        Assert.Equal("1", message.Data["tag"]);
+    }
+}
