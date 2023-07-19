dc2f130289b9e6884311d7cff4e0bef9fb0f4716
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2023-07-19 12:17:13 +0200

    initial commit

---
+++ FCMPN/Program.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+using FirebaseAdmin.Messaging;
+using Microsoft.Extensions.Configuration;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = new ConfigurationBuilder();
+        builder.AddUserSecrets<Program>();
+        var config = builder.Build();
+
+        var deviceToken = config["DeviceToken"];
+        var accRef = config["AccRef"];
+
+        FirebaseApp.Create(new AppOptions()
+        {
+            Credential = GoogleCredential.FromFile("/Users/nikolavetnic/Documents/Texts/j&s-soft/aduwuvovud.json"),
+        });
+
+        var content = new
+        {
+            accRef = accRef,
+            eventName = "dynamic",
+            payload = new
+            {
+                someProperty = "someValue"
+            },
+            sentAt = "2021-01-01T00:00:00.000Z",
+        };
+
+        var serializedContent = JsonConvert.SerializeObject(content);
+
+        var message = new Message()
+        {
+            Data = new Dictionary<string, string>()
+            {
+                { "content", serializedContent },
+            },
+            Token = deviceToken,
+        };
+
+        var response = await FirebaseMessaging.DefaultInstance.SendAsync(message);
+        Console.WriteLine("Successfully sent message: " + response);
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Firebase Cloud Messaging Push Notifications Example
+
+This is an example of using Firebase Cloud Messaging in C# to send a notification to a specific device running the Enmeshed app.
+
+This code uses **Firebase Admin SDK** to send the notification. It is the desired way to send notifications to the Enmeshed app as it can send the content objects as part of notifications which the app can then process.
+
+This is a working example on 19 July 2023.
